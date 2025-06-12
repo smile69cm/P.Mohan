f4f9e36018c771949b00c393706652bea3ad3336
smile69cm <smile69cm@gmail.com>
2025-06-12 18:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+AOS.init();
+
+function hideLoader() {
+  document.getElementById('loader-wrapper').style.display = 'none';
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
